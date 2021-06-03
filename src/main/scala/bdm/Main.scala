2731f9db02e8ad9a2cdd835f0d84917c77e1ed9c
Arnau Abella <arnauabella@gmail.com>
2021-06-03 16:23:27 +0200

    modeling finished

--- src/main/scala/bdm/Main.scala
+++ src/main/scala/bdm/Main.scala
@@ -1,7 +1,15 @@
 package bdm
 
 import org.apache.spark.sql.SparkSession
-import scala.math.random
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.feature.StringIndexer
+import org.apache.spark.ml.feature.StandardScaler
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.regression.LinearRegression
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.feature.OneHotEncoder
+
+case class Sample(neigh: String, price: Double, rfd: Double)
 
 object Main extends App {
   val spark: SparkSession =
@@ -10,19 +18,74 @@ object Main extends App {
       .master("local[4]")
       .appName("myApp")
       .getOrCreate()
-  spark.sparkContext.setLogLevel("ERROR")
-  val slices = if (args.length > 0) args(0).toInt else 2
-  val n = math.min(100000L * slices, Int.MaxValue).toInt
-  val count = spark.sparkContext
-    .parallelize(1 until n, slices)
-    .map { _ =>
-      val x = random() * 2 - 1
-      val y = random() * 2 - 1
-      if (x * x + y * y <= 1) 1 else 0
-    }
-    .reduce(_ + _)
 
-  println(s"Pi is roughly ${4.0 * count / (n - 1)}")
+  spark.sparkContext.setLogLevel("ERROR")
+
+  import spark.implicits._
+
+  val df = spark.read
+    .option("header", true)
+    .csv("datasets/p3_integrated.csv")
+
+  val data = df.map { row =>
+    val neigh = row.getAs[String]("neigh_id")
+    val price = row.getAs[String]("price").toDouble
+    val rfd = row.getAs[String]("RFD").toDouble
+    Sample(neigh, price, rfd)
+  }
+
+  val Array(training, test) = data.randomSplit(Array(0.7, 0.3))
+
+  val assembler1 = new VectorAssembler()
+    .setInputCols(Array("price"))
+    .setOutputCol("numericals")
+
+  val scaler = new StandardScaler()
+    .setInputCol("numericals")
+    .setOutputCol("numericals_std")
+    .setWithStd(true)
+    .setWithMean(true)
+
+  val indexer = new StringIndexer()
+    .setInputCol("neigh")
+    .setOutputCol("neigh_cat")
+    .setHandleInvalid("keep")
+
+  // You can't apply one hot encoding without StringIndexer first.
+  val oneHot = new OneHotEncoder()
+    .setInputCol("neigh_cat")
+    .setOutputCol("categoricals")
+
+  val assembler2 = new VectorAssembler()
+    .setInputCols(Array("numericals_std", "categoricals"))
+    .setOutputCol("features")
+
+  val lr = new LinearRegression()
+    .setLabelCol("rfd") // target
+    .setFeaturesCol("features")
+    .setMaxIter(100)
+    .setRegParam(0.1)
+
+  val pipeline = new Pipeline()
+    .setStages(Array(assembler1, indexer, oneHot, scaler, assembler2, lr))
+
+  val model = pipeline.fit(training)
+
+  model.write.overwrite().save("datasets/lr-model")
+
+  // val model2 = PipelineModel.load("datasets/lr-model")
+
+  val predictions = model.transform(test)
+
+  predictions.select("neigh", "price", "prediction").show(5)
+
+  val evaluator = new RegressionEvaluator()
+    .setLabelCol("rfd")
+    .setPredictionCol("prediction")
+    .setMetricName("rmse")
+
+  val rmse = evaluator.evaluate(predictions)
+  println(s"RMSE: $rmse")
 
   spark.stop()
 }
